ce60e6f24fe8dd306713f12adb492713f367266e
Bill Casey <billc@smu.edu>
2016-08-07 21:09:50 -0500

    Adding More Select Statements
    More Select Statements and edited descriptions.

--- Sample_Queries.sql
+++ Sample_Queries.sql
@@ -6,10 +6,21 @@ SELECT * FROM Mission;
 /* Show all the missions, but only show mission's name */
 SELECT Mission_Name FROM Mission;
 
-/* Print all the project mercury missions with flight details */
+/* Show all the Project Mercury missions with flight details */
 SELECT * FROM Mission
 WHERE Mission_Name LIKE 'Mercury%';
 
-/* Print all the project mercury mission names only */
+/* Only show Project Mercury mission names */
 SELECT Mission_Name FROM Mission
 WHERE Mission_Name LIKE 'Mercury%';
+
+/* Show all astronauts */
+SELECT * FROM Astronaut;
+
+/* Show only astronauts with Nicknames */
+SELECT * FROM Astronaut
+WHERE Nickname IS NOT NULL;
+
+/* Select only astronauts with nicknames, then show them as Nickname, Lastname */
+SELECT Nickname, Last_Name FROM Astronaut
+WHERE Nickname IS NOT NULL;
