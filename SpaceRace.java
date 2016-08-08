bdda0fcf36f375c74b85eb45234d7cf303be4ab5
Bill Casey <billc@smu.edu>
2016-08-07 20:26:12 -0500

    Initializing the Repository
    Creating the project and initializing the repository.

---
+++ SpaceRace.java
@@ -0,0 +1,23 @@
+package spaceRaceSQL;
+
+/**
+ *  A java application to learn about JDBC and MySQL using sample data from 
+ *  NASA missions in the 1960's and 70's. 
+ * 
+ *  @author Bill Casey
+ */
+public class SpaceRace
+{
+
+    public SpaceRace()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Bill
+ *
+ */
+package spaceRaceSQL;
