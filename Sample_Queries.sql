9e3ccfa2c839980547f5d8f63c8aa1fd7ed5b268
Bill Casey <billc@smu.edu>
2016-08-07 21:20:56 -0500

    Addition Select Statements and Joins
    More advanced SELECT statements along with some LEFT JOINS.

--- Sample_Queries.sql
+++ Sample_Queries.sql
@@ -1,5 +1,6 @@
 /* These sample queries will help you understand SQL SELECTS and JOINS */
 
+--SELECT STATEMENTS ONLY
 /* Show all the missions, and show all the mission information */
 SELECT * FROM Mission;
 
@@ -24,3 +25,33 @@ WHERE Nickname IS NOT NULL;
 /* Select only astronauts with nicknames, then show them as Nickname, Lastname */
 SELECT Nickname, Last_Name FROM Astronaut
 WHERE Nickname IS NOT NULL;
+
+/* Select only astronauts who flew in space */
+SELECT First_Name, Last_Name FROM Astronaut
+WHERE Astronaut_ID IN 
+(
+	SELECT Astronaut_ID FROM MissionAstronaut
+);
+
+/* Astronauts who did not fly in space */
+SELECT First_Name, Last_Name FROM Astronaut
+WHERE Astronaut_ID NOT IN 
+(
+	SELECT Astronaut_ID FROM MissionAstronaut
+);
+
+--JOINS
+/* Prints All Missions with the Astronauts that flew on them */ 
+SELECT Mission.Mission_Name, Astronaut.First_Name, Astronaut.Last_Name FROM MissionAstronaut
+LEFT JOIN Astronaut
+	ON MissionAstronaut.Astronaut_ID = Astronaut.Astronaut_ID
+LEFT Join Mission
+	ON MissionAstronaut.Mission_ID = Mission.Mission_ID;
+	
+/* Select all Astronauts who flew on a specific mission by its Mission_ID */
+SELECT Mission.Mission_Name, Astronaut.First_Name, Astronaut.Last_Name FROM MissionAstronaut
+INNER JOIN Astronaut
+	ON MissionAstronaut.Astronaut_ID = Astronaut.Astronaut_ID
+INNER JOIN Mission
+	ON MissionAstronaut.Mission_ID = Mission.Mission_ID
+	AND Mission.Mission_ID = 22; /* 22 was Apollo 11 */
